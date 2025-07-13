56d589c955bf0b7ce15493d8ffc689121b302874
Srijavali <kothasrijavali@gmail.com>
2025-07-13 19:40:19 +0530

    Add files via upload

---
+++ src/def.js
@@ -0,0 +1,23 @@
+// def.js
+
+import Homepage from './Homepage';
+import Loginpage from './Loginpage';
+import MarineInterface from './marineinterface';
+import Starfishgame from './Starfishgame';
+import GameReportGenerator from './GameReportGenerator';
+import TherapistDashboard from './components/TherapistDashboard';
+import TherapistListPage from './TherapistListPage';
+import TherapistStatusPage from './TherapistStatusPage';
+import { SuperAdminDashboard } from './SuperAdminDashboard';
+
+export {
+  Homepage,
+  Loginpage,
+  MarineInterface,
+  Starfishgame,
+  GameReportGenerator,
+  TherapistDashboard,
+  TherapistListPage,
+  TherapistStatusPage,
+  SuperAdminDashboard,
+};
